94c230fbd0ae154fe0fe03293748e68f34e842db
danielsilvestre2 <mephilesthedark650@gmail.com>
2025-08-16 11:51:29 -0300

    resolve bug on menu bar

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+function menuSize() {
+  if (window.innerWidth >= 650) {
+    navtop.style.display = 'block'
+  }
+  else {
+    navtop.style.display = 'none'
+  }
+}
+
+
 function clickMenu() {
   if (navtop.style.display == 'block') {
     navtop.style.display = 'none'
